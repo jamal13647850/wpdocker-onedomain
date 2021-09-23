3f698c42561da287a8dfa3dc760103fe8ade4eae
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-09-23 23:12:29 +0330

    Add mail changer

--- config.sh
+++ config.sh
@@ -3,7 +3,6 @@
 
 echo "Set the domain for this project"
 
-existing="example.com"
 echo "New domain:(eg newdomain.com)"
 read replacement
 echo "Replacing all occurences of $existing with $replacement in files matching $filepattern"
@@ -11,5 +10,11 @@ echo "Replacing all occurences of $existing with $replacement in files matching 
 #find . -name '*.conf' -o -name '*.yml' -print0 | xargs -0 sed -i '' -e "s/$existing/$replacement/g"
 
 for filename in `find . -name '*.conf' -o -name '*.yml'`; do
-  sed -i "s/$existing/$replacement/g" $filename
+  sed -i "s/example.com/$replacement/g" $filename
+done
+
+echo "Your email for letsencrypt:"
+read email
+for filename in `find . -name '*.conf' -o -name '*.yml'`; do
+  sed -i "s/yourmail@gmail.com/$email/g" $filename
 done
