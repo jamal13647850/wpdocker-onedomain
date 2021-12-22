1800c5954f669b96626e614629680edf67f0cebf
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-12-22 21:23:07 +0330

    Add wp-cron

--- setCronJobs.sh
+++ setCronJobs.sh
@@ -9,6 +9,8 @@ read logpath
 crontab -l > mycron
 #echo new cron into cron file
 echo "0 0 3 * * docker restart certbot >> $logpath/certbot.log 2>&1" >> mycron
+echo "0 */12 * * * docker exec wordpress /usr/sbin/crond" >> mycron
+echo "@reboot docker exec wordpress /usr/sbin/crond" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
