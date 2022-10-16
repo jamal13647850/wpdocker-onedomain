517866a7ffef2dae35de437ee466680cf2c574f4
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2022-10-16 09:45:43 +0330

    Add arvancloud support

--- nginx/helpers/arvancloud-ip-sync.sh
+++ nginx/helpers/arvancloud-ip-sync.sh
@@ -2,20 +2,14 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ARVANCLOUD_FILE_PATH=${1:-$SCRIPT_DIR'/arvancloud.conf'}
-echo $ARVANCLOUD_FILE_PATH;
-echo "#Arvancloud" > $ARVANCLOUD_FILE_PATH;
+
+echo "#ArvanCloud" > $ARVANCLOUD_FILE_PATH;
 echo "" >> $ARVANCLOUD_FILE_PATH;
 
 echo "# - IPv4" >> $ARVANCLOUD_FILE_PATH;
 for i in `curl https://www.arvancloud.com/fa/ips.txt`; do
-        echo "set_real_ip_from $i;" >> $ARVANCLOUD_FILE_PATH;
+    echo "set_real_ip_from $i;" >> $ARVANCLOUD_FILE_PATH;
 done
 
-# echo "" >> $ARVANCLOUD_FILE_PATH;
-# echo "# - IPv6" >> $ARVANCLOUD_FILE_PATH;
-# for i in `curl https://www.arvancloud.com/ips-v6`; do
-#         echo "set_real_ip_from $i;" >> $ARVANCLOUD_FILE_PATH;
-# done
-
 echo "" >> $ARVANCLOUD_FILE_PATH;
-echo "real_ip_header AR-Connecting-IP;" >> $ARVANCLOUD_FILE_PATH;
+echo "real_ip_header ar-real-ip;" >> $ARVANCLOUD_FILE_PATH;
